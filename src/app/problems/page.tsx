dfbca44ffdab0f41634d76a11766bf1d7133ce68
kvqn <guneetaggarwal@gmail.com>
2024-09-20 18:23:44 +0530

    feat: markdown rendering

--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -4,11 +4,14 @@ import { Input } from "@/components/ui/input"
 import { runInput } from "@/server/actions/run-input"
 import { useState } from "react"
 import { toast } from "sonner"
+import Markdown from "@/problems/hello-world/writeup.mdx"
 
 export default function Page() {
   const [input, setInput] = useState("")
   return (
     <div>
+      <Markdown />
+
       <Input
         placeholder="Enter your script"
         value={input}
