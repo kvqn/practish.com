076b84b275e71b8b49eda2f49360d332df4195a8
kvqn <guneetaggarwal@gmail.com>
2025-01-28 23:44:01 +0530

    ui updates and migrate to bun text based lock file

--- src/app/problems/_components/testcase-terminal.tsx
+++ src/app/problems/_components/testcase-terminal.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { getTerminalSession } from "@/server/actions/get-terminal-session"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useProblem } from "./problem-context"
 import { Input } from "@/components/ui/input"
 import { submitTerminalSessionCommand } from "@/server/actions/submit-terminal-session-command"
@@ -15,6 +15,8 @@ export function TestcaseTerminal({ testcase }: { testcase: number }) {
 
   const [input, setInput] = useState("")
   const [running, setRunning] = useState(false)
+  const inputRef = useRef<HTMLInputElement>(null)
+  const terminalRef = useRef<HTMLDivElement>(null)
 
   async function handleSubmit() {
     if (!session) return
@@ -30,8 +32,22 @@ export function TestcaseTerminal({ testcase }: { testcase: number }) {
         logs: [...prev.logs, log],
       }
     })
+    setInput("")
     setRunning(false)
   }
+
+  useEffect(() => {
+    terminalRef.current?.scrollTo({
+      top: terminalRef.current.scrollHeight,
+      behavior: "smooth",
+    })
+  }, [session])
+
+  useEffect(() => {
+    if (!running) {
+      inputRef.current?.focus()
+    }
+  }, [running])
 
   useEffect(() => {
     void (async () => {
@@ -41,28 +57,47 @@ export function TestcaseTerminal({ testcase }: { testcase: number }) {
       })
       setSession(session)
     })()
-  }, [])
+  }, [problemId, testcase])
 
   if (!session) return <div>loading</div>
 
   return (
-    <div>
-      <div className="flex flex-col">
+    <div className="flex flex-col font-geist-mono">
+      <div
+        className="flex h-80 flex-col overflow-scroll bg-black px-2 py-1"
+        ref={terminalRef}
+      >
         {session.logs.map((log) => (
           <div key={log.id}>
-            <p>{`>>> ${log.stdin}`}</p>
-            {log.stdout.length > 0 && <p>{log.stdout}</p>}
-            {log.stderr.length > 0 && <p>{log.stderr}</p>}
+            <p className="text-white">{`>>> ${log.stdin}`}</p>
+            {log.stdout.length > 0 && (
+              <p className="text-neutral-400">{log.stdout}</p>
+            )}
+            {log.stderr.length > 0 && (
+              <p className="text-red-600">{log.stderr}</p>
+            )}
           </div>
         ))}
       </div>
-      <div>
-        <Input
+      <div className="flex">
+        <input
+          ref={inputRef}
           value={input}
           onChange={(e) => setInput(e.target.value)}
           disabled={running}
+          className="flex-grow bg-neutral-800 px-2 py-1 text-white outline-none"
+          autoFocus
+          onKeyDown={async (e) => {
+            if (e.key === "Enter") {
+              await handleSubmit()
+            }
+          }}
         />
-        <button onClick={handleSubmit} disabled={running}>
+        <button
+          onClick={handleSubmit}
+          disabled={running}
+          className="bg-green-800 px-2 text-neutral-200 hover:bg-green-700"
+        >
           submit
         </button>
       </div>
