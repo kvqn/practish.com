54a05929af9fc5ac4017e7f441238bc7094d94df
kvqn <guneetaggarwal@gmail.com>
2025-02-13 19:25:27 +0530

    feat: homepage

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,3 +10,17 @@ export function cn(...inputs: ClassValue[]) {
 export async function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
+
+export function min(...numbers: number[]): number {
+  if (numbers.length === 0) {
+    return Infinity // Or throw an error, depending on desired behavior
+  }
+
+  let minimum = numbers[0]!
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i]! < minimum!) {
+      minimum = numbers[i]!
+    }
+  }
+  return minimum
+}
