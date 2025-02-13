b336302ad02fbc81625b3df0fd6cc8472db562ac
kvqn <guneetaggarwal@gmail.com>
2025-02-13 19:48:47 +0530

    style: placeholder browse page

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -2,10 +2,27 @@
 
 import { getProblemInfo } from "@/server/actions/get-problem-info"
 import { getProblems } from "@/server/actions/get-problems"
+import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
 export default function Page() {
+  return (
+    <div className="flex flex-col items-center gap-8">
+      <div
+        className="w-full bg-gray-50 p-20"
+        style={{
+          backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%239C92AC' fill-opacity='0.4' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E")`,
+        }}
+      >
+        <h1 className="text-center text-6xl font-bold">Browse Problems</h1>
+      </div>
+      <Table />
+    </div>
+  )
+}
+
+function Table() {
   const [problems, setProblems] = useState<Awaited<
     ReturnType<typeof getProblems>
   > | null>(null)
@@ -16,11 +33,9 @@ export default function Page() {
     })()
   }, [])
 
-  if (!problems) return <div>Loading...</div>
-
-  return (
-    <div className="mx-auto w-1/2">
-      <table className="table-auto divide-y rounded-xl border">
+  if (!problems)
+    return (
+      <table className="w-1/2 table-auto divide-y rounded-xl border">
         <thead>
           <tr className="divide-x *:p-2">
             <th>#</th>
@@ -29,12 +44,69 @@ export default function Page() {
           </tr>
         </thead>
         <tbody className="divide-y">
-          {problems.map((problem) => (
-            <Problem key={problem} problem={problem} />
-          ))}
+          <tr className="h-8 divide-x *:p-2">
+            <td>
+              <p className="h-2 animate-pulse rounded bg-gray-200" />
+            </td>
+            <td>
+              <p className="h-2 animate-pulse rounded bg-gray-200" />
+            </td>
+            <td>
+              <p className="h-2 animate-pulse rounded bg-gray-200" />
+            </td>
+          </tr>
+          <tr className="h-8 divide-x *:p-2">
+            <td>
+              <p className="h-2 animate-pulse rounded bg-gray-200" />
+            </td>
+            <td>
+              <p className="h-2 animate-pulse rounded bg-gray-200" />
+            </td>
+            <td>
+              <p className="h-2 animate-pulse rounded bg-gray-200" />
+            </td>
+          </tr>
+          <tr className="h-8 divide-x *:p-2">
+            <td>
+              <p className="h-2 animate-pulse rounded bg-gray-200" />
+            </td>
+            <td>
+              <p className="h-2 animate-pulse rounded bg-gray-200" />
+            </td>
+            <td>
+              <p className="h-2 animate-pulse rounded bg-gray-200" />
+            </td>
+          </tr>
+          <tr className="h-8 divide-x *:p-2">
+            <td>
+              <p className="h-2 animate-pulse rounded bg-gray-200" />
+            </td>
+            <td>
+              <p className="h-2 animate-pulse rounded bg-gray-200" />
+            </td>
+            <td>
+              <p className="h-2 animate-pulse rounded bg-gray-200" />
+            </td>
+          </tr>
         </tbody>
       </table>
-    </div>
+    )
+
+  return (
+    <table className="w-1/2 table-auto divide-y rounded-xl border">
+      <thead>
+        <tr className="divide-x *:p-2">
+          <th>#</th>
+          <th>Title</th>
+          <th>Solved</th>
+        </tr>
+      </thead>
+      <tbody className="divide-y">
+        {problems.map((problem) => (
+          <Problem key={problem} problem={problem} />
+        ))}
+      </tbody>
+    </table>
   )
 }
 
@@ -51,10 +123,16 @@ function Problem({ problem }: { problem: string }) {
 
   if (!info)
     return (
-      <tr>
-        <td className="animate-pulse"></td>
-        <td></td>
-        <td></td>
+      <tr className="h-8 divide-x *:p-2">
+        <td>
+          <p className="h-2 animate-pulse rounded bg-gray-200" />
+        </td>
+        <td>
+          <p className="h-2 animate-pulse rounded bg-gray-200" />
+        </td>
+        <td>
+          <p className="h-2 animate-pulse rounded bg-gray-200" />
+        </td>
       </tr>
     )
 
@@ -62,12 +140,12 @@ function Problem({ problem }: { problem: string }) {
 
   return (
     <tr
-      className="cursor-pointer divide-x *:p-2"
+      className="cursor-pointer divide-x transition-colors *:p-2 hover:bg-gray-100"
       onClick={() => {
         router.push(`/problems/${info.slug}`)
       }}
     >
-      <td>{info.id}</td>
+      <td className="text-center">{info.id}</td>
       <td>{info.slug}</td>
       <td>No</td>
     </tr>
