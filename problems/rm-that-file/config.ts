1b529105c9d8852041075138be83653e30e69c62
kvqn <guneetaggarwal@gmail.com>
2025-02-18 13:07:11 +0530

    feat: problem (rm-that-file)

---
+++ problems/rm-that-file/config.ts
@@ -0,0 +1,45 @@
+import { getFs } from "@/server/utils/paths"
+import type { FsType, ProblemConfig } from "@/server/utils/problem"
+
+const SLUG = "rm-that-file"
+
+async function testcaseConfig({
+  id,
+  isPublic,
+}: {
+  id: number
+  isPublic: boolean
+}) {
+  const originalFs = await getFs(`./problems/${SLUG}/testcases/${id}`)
+  const newFs: FsType = {}
+  for (const file in originalFs) {
+    if (file !== "secrets/portal.txt") {
+      newFs[file] = originalFs[file]!
+    }
+  }
+
+  return {
+    id: id,
+    folder: `${id}`,
+    public: isPublic,
+    expected_fs: newFs,
+  }
+}
+
+const config: ProblemConfig = {
+  id: 7,
+  slug: SLUG,
+  title: "Remove That File",
+  description: `A single file is standing in the way—time to delete it and clear the clutter! 🗑️`,
+  tags: ["Basics"],
+  capture_stdout: false,
+  capture_stderr: false,
+  capture_exit_code: false,
+  capture_fs: true,
+  testcases: [
+    await testcaseConfig({ id: 1, isPublic: true }),
+    await testcaseConfig({ id: 2, isPublic: true }),
+  ],
+}
+
+export default config
