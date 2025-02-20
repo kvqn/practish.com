0877461101a4b05838cb7f419fc8a182d9c85ee5
kvqn <guneetaggarwal@gmail.com>
2025-02-20 19:48:13 +0530

    feat: login using github and google + styling

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,10 @@ import {
   type NextAuthOptions,
 } from "next-auth"
 import { type Adapter } from "next-auth/adapters"
+
 import DiscordProvider from "next-auth/providers/discord"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
 
 import { env } from "@/env"
 import { db } from "@/server/db"
@@ -63,6 +66,14 @@ export const authOptions: NextAuthOptions = {
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
+    }),
+    GithubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    }),
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
     /**
      * ...add more providers here.
